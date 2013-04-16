a51fb23c7a85e4c3477d6f4b09457d6daf714337
Adam Hakowski <adam.hakowski@gmail.com>
2013-04-16 21:19:54 +0200

    NEW CONSOLE AND GUI

---
+++ SOB_TEST_GUI/SOB_TEST_GUI/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace SOB_TEST_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tBid = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tBname = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tBage = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.bAZAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ączToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Połącz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(188, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(136, 238);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NAME:";
+            // 
+            // tBid
+            // 
+            this.tBid.Location = new System.Drawing.Point(80, 197);
+            this.tBid.Name = "tBid";
+            this.tBid.Size = new System.Drawing.Size(100, 20);
+            this.tBid.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ID:";
+            // 
+            // tBname
+            // 
+            this.tBname.Location = new System.Drawing.Point(80, 223);
+            this.tBname.Name = "tBname";
+            this.tBname.Size = new System.Drawing.Size(100, 20);
+            this.tBname.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "AGE:";
+            // 
+            // tBage
+            // 
+            this.tBage.Location = new System.Drawing.Point(80, 249);
+            this.tBage.Name = "tBage";
+            this.tBage.Size = new System.Drawing.Size(100, 20);
+            this.tBage.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(80, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bAZAToolStripMenuItem,
+            this.programToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(822, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // bAZAToolStripMenuItem
+            // 
+            this.bAZAToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ączToolStripMenuItem});
+            this.bAZAToolStripMenuItem.Name = "bAZAToolStripMenuItem";
+            this.bAZAToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.bAZAToolStripMenuItem.Text = "BAZA";
+            // 
+            // ączToolStripMenuItem
+            // 
+            this.ączToolStripMenuItem.Name = "ączToolStripMenuItem";
+            this.ączToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.ączToolStripMenuItem.Text = "Połącz";
+            this.ączToolStripMenuItem.Click += new System.EventHandler(this.ączToolStripMenuItem_Click);
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zamknijToolStripMenuItem});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.programToolStripMenuItem.Text = "Program";
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.zamknijToolStripMenuItem.Text = "Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.zamknijToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(371, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 150);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 336);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tBage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tBname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tBid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tBid;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tBname;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tBage;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem bAZAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ączToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SOB_TEST_GUI/SOB_TEST_GUI/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace SOB_TEST_GUI
+{
+    public partial class Form1 : Form
+    {
+        public string ConnecitonString = "Server=192.168.1.15;Database=SOB_test;Uid=sob;Pwd=sob12;";
+   
+        public MySqlConnection conn;
+        public MySqlCommand cmd;
+        public MySqlDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(ConnecitonString);
+            cmd = new MySqlCommand("", conn);
+            dataGridView1.Columns.Add("id","id");
+            dataGridView1.Columns.Add("name","name");
+            dataGridView1.Columns.Add("age", "age");
+
+        }
+        public  bool refresh() 
+        {
+            listBox1.Items.Clear();
+           
+                
+                try
+                {
+                    conn.Open();
+                    cmd.CommandText = "SELECT * FROM test";
+                    
+
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        string thisrow = "";
+                        for (int i = 0; i < dr.FieldCount; i++)
+                        {
+                            thisrow += dr.GetValue(i).ToString() + " ";
+                     
+                        }
+                            listBox1.Items.Add(thisrow);
+                            
+                    }
+            
+                    conn.Close();
+                    return true;
+                }
+                catch (MySqlException expsql2)
+                {
+                    MessageBox.Show(String.Format("nie udalo sie pobrac danych, blad{0}", expsql2), "Pobieranie danych", MessageBoxButtons.OK);
+
+                    return false;
+                }
+            
+            
+            
+        }
+        public bool dodaj() 
+        {
+            try
+            {
+                conn.Open();
+                cmd.CommandText = "insert into test(id,name,age) value('" + tBid.Text + "','" + tBname.Text + "','" + tBage.Text + "')";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                refresh();
+                return true;
+            }
+            catch (MySqlException expsql3) 
+            {
+                MessageBox.Show(String.Format("Błąd {0}", expsql3),"Info", MessageBoxButtons.OK);
+                return false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+               if( refresh())
+               MessageBox.Show("polaczona z baza danych", "połaczenie", MessageBoxButtons.OK); 
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                if (tBage.Text == "" | tBname.Text == "" | tBid.Text == "")
+                {
+                    MessageBox.Show("wszystkie pola musza zostać wypełnione", "błąd", MessageBoxButtons.OK);
+                    return;
+                }
+                
+              if(dodaj())  
+                MessageBox.Show("Rekord zostal dodanny");
+        
+        }
+
+        private void ączToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
